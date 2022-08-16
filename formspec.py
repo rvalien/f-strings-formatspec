56aaae2a5571fccdd9a7f1adbf1e80b2d8a5a916
Valien R <34721834+rvalien@users.noreply.github.com>
2022-08-16 22:54:57 +0300

    add date formats

--- formspec.py
+++ formspec.py
@@ -1,5 +1,6 @@
 # https://docs.python.org/3/library/string.html#formatspec
 # https://strftime.org/
+import datetime
 
 text = "hello world"
 
@@ -15,29 +16,40 @@ f"{text:*<15}"
 
 big_num = 1234567890
 # Set separator
-print(f"{number:_}")
+print(f"{big_num:_}")
 # '1_234_567_890'
-print(f"{number:,}")
+print(f"{big_num:,}")
 # '1,234,567,890'
 
-float_num = 2343552.6516251625
-print(f'{float_num:,.3f}')
+big_float = 2343552.6516251625
+print(f'{big_float:,.3f}')
 # '2,343,552.652'
 
+# Time
+now = datetime.datetime.utcnow()
+print(f'{now:%F}')
+# 2022-08-16
+print(f'{now:%T}')
+# 19:52:00
+print(f'{now:%X}')
+# 19:50:26
+print(f'{now:%x}')
+# 08/16/22
 
-0.4342105263157895
-f'{p:%}'
+# Float
+p = 0.4342105263157895
+print(f'{p:%}')
 # '43.421053%'
-f'{p:.2%}'
+print(f'{p:.2%}')
 # '43.42%'
 
 
-
-number = 123
 # Add leading zeros
+number = 123
 print(f"{number:08}")
 # 00000123
 
 
-f"{some_obj!r}" 
-#- __repr__
+# f"{some_obj!r}"
+# - __repr__
+
